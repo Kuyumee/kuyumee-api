ce199003a50c718d90218fa6f771137e142448cc
Cwickks <aaronfilarcaa@gmail.com>
2022-08-26 17:57:10 +0800

    cors

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const { echo } = require("./server/api/echo.js");
 
 require("./server/helpers/db.js");
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use("/", function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
   next();
 });
 
