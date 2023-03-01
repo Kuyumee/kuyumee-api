f7848ac2a82e60ff9b8ae14e6375d726cdf6da66
Cwickks <69566792+Cwickks@users.noreply.github.com>
2023-03-01 20:01:13 +0800

    fix all

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 console.log("Starting...");
 
 require("dotenv").config();
-require("./helpers/db.js").init();
-
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
 const express = require("express");
 const app = express();
 
 const multer = require("multer");
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ dest: os.tmpdir() });
 
 const archiver = require("archiver");
 archiver.registerFormat("zip-encrypted", require("archiver-zip-encrypted"));
 
+const db = require("./helpers/db.js").init();
 const { animetracker } = require("./api/animetracker.js");
-const db = require("./helpers/db.js");
 
 app.use("/", function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -26,53 +25,55 @@ app.use("/", function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => res.end("OK"));
+app.get("/", (req, res) => {
+  res.end("OK");
+});
 
 app.get("/api/anime-tracker", async (req, res) => {
   try {
-    if (req.query.key !== process.env.KEY) return res.sendStatus(403);
+    if (req.query.key !== process.env.KEY) {
+      return res.sendStatus(403);
+    }
     const result = await animetracker(req.query.f, req.query.d);
     res.send(result);
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.log(error);
     res.sendStatus(500);
   }
 });
 
 app.post("/upload", upload.array("files"), async (req, res) => {
   try {
-    const bucket = db.getBucket();
-
-    if (req.files.length === 0) {
+    if (!req.files) {
       return res.status(400).send("No files were uploaded.");
-    } else {
-      const filename = `${Date.now()}.zip`;
-      const output = fs.createWriteStream(__dirname + `/.${filename}`);
-
-      output.on("close", (e) => {
-        fs.renameSync(__dirname + `/.${filename}`, __dirname + `/${filename}`);
-      });
-
-      const archive = archiver("zip-encrypted", {
-        zlib: { level: 9 },
-        encryptionMethod: "aes256",
-        password: process.env.KEY,
-      });
-
-      archive.pipe(output);
-
-      for (const file of req.files) {
-        archive.append(file.buffer, { name: file.originalname });
-      }
-
-      archive.finalize();
     }
 
+    const filename = `${Date.now()}.zip`;
+    const output = fs.createWriteStream(path.join(__dirname, `./.${filename}`));
+
+    output.on("close", () => {
+      fs.renameSync(path.join(__dirname, `./.${filename}`), path.join(__dirname, `./${filename}`));
+    });
+
+    const archive = archiver("zip-encrypted", {
+      zlib: { level: 9 },
+      encryptionMethod: "aes256",
+      password: process.env.KEY,
+    });
+
+    archive.pipe(output);
+
+    for (const file of req.files) {
+      archive.append(fs.createReadStream(file.path), { name: file.originalname });
+    }
+
+    archive.finalize();
+
     res.send("Files uploaded successfully");
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
     res.status(500).send("Error uploading files");
   }
 });
 
-app.listen(process.env.PORT, () => console.log(`Ready!`));
+app.listen(process.env.PORT, () => console.log("Ready!"));
