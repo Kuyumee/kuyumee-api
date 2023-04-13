92cb5bb4b92e3f5644bb53d8ff7339cb77768194
Cwickks <69566792+Cwickks@users.noreply.github.com>
2023-04-13 19:28:24 +0800

    only show correct episodes

--- api/animetracker.js
+++ api/animetracker.js
@@ -6,58 +6,61 @@ async function animetracker(req, res) {
   if (req.query.f === "home") {
     const watching = await axios(`https://api.myanimelist.net/v2/users/kuyumee/animelist?nsfw=1&status=watching&limit=1000`, { headers: { "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID } }).then((a) => a.data.data);
 
-    let result = watching.map((a) => ({ title: a.node.title, nyaa: a.node.title.slice(0, 8), main_picture: a.node.main_picture.medium, episodes: [] }));
+    let animes = watching.map((a) => ({ title: a.node.title, nyaaTitle: null, searchKey: a.node.title.slice(1, 8), main_picture: a.node.main_picture.medium, episodes: [] }));
 
-    const magnets = await si.searchAll({ term: `[ASW] "${result.map((a) => a.nyaa).join('"|"')}"` });
+    const animeMagnets = await si.searchAll({ term: `[ASW] "${animes.map((a) => a.searchKey).join('"|"')}"` });
 
-    for (const magnet of magnets.reverse()) {
-      const titleMatch = magnet.name.match(/\[ASW\] (.+) - (?:(?<=- )[^-]+(?= \[))/);
-      const episodeMatch = magnet.name.match(/(?<=- )([^-]+)(?= \[)/);
+    for (const animeMagnet of animeMagnets) {
+      const titleMatch = animeMagnet.name.match(/\[ASW\] (.+) - (?:(?<=- )[^-]+(?= \[))/);
+      const episodeMatch = animeMagnet.name.match(/(?<=- )([^-]+)(?= \[)/);
 
-      if (!titleMatch || !episodeMatch || episodeMatch[1].length >= 5) continue;
+      if (!titleMatch || !episodeMatch || episodeMatch[1].length > 4) continue;
 
       const title = titleMatch[1];
       const episode = episodeMatch[1];
-      const magnetLink = magnet.magnet;
 
-      const index = result.findIndex((a) => title.includes(a.nyaa));
+      const index = animes.findIndex((a) => title === a.nyaaTitle || (title.includes(a.searchKey) && a.nyaaTitle === null));
       if (index === -1) continue;
 
-      const dateCreated = new Date(magnet.date).getTime();
-
-      result[index].episodes.push({ episode, magnetLink, dateCreated, status: 0 });
+      animes[index].nyaaTitle = title;
+      animes[index].episodes.push({
+        episode,
+        magnetLink: animeMagnet.magnet,
+        dateCreated: new Date(animeMagnet.date).getTime(),
+        status: 0,
+      });
     }
 
-    result = result.filter((a) => a.episodes.length > 0);
+    animes = animes.filter((a) => a.episodes.length > 0);
+    animes = animes.map((a) => ({ ...a, episodes: a.episodes.reverse() }));
 
-    const titles = result.map((a) => a.title);
     const dbResult = await db
       .collection("animetracker")
-      .find({ _id: { $in: titles } })
+      .find({ _id: { $in: animes.map((a) => a.title) } })
       .toArray();
 
-    for (const anime of dbResult) {
-      const index = result.findIndex((a) => a.title === anime._id);
+    for (const animeDbEntry of dbResult) {
+      const index = animes.findIndex((a) => a.title === animeDbEntry._id);
       if (index === -1) continue;
-      for (const episode of anime.episodes) {
-        const episodeIndex = result[index].episodes.findIndex((a) => a.episode == episode);
+      for (const episode of animeDbEntry.episodes) {
+        const episodeIndex = animes[index].episodes.findIndex((a) => a.episode == episode);
         if (episodeIndex === -1) continue;
-        result[index].episodes[episodeIndex].status = 1;
+        animes[index].episodes[episodeIndex].status = 1;
       }
     }
 
     // Sorting: Top code should be the least priority
     // Sort by last episode date is first
-    result = result.sort((a, b) => {
+    animes = animes.sort((a, b) => {
       return b.episodes.at(-1).dateCreated - a.episodes.at(-1).dateCreated;
     });
 
     // Sort unwatched is first
-    result = result.sort((a, b) => {
+    animes = animes.sort((a, b) => {
       return a.episodes.at(-1).status - b.episodes.at(-1).status;
     });
 
-    res.json(result);
+    res.json(animes);
   } else if (req.query.f === "update") {
     const anime = JSON.parse(req.query.d);
     await db.collection("animetracker").updateOne({ _id: anime.title }, { $addToSet: { episodes: anime.episode } }, { upsert: true });
