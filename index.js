e73f5fd544df2984e513d6338d85677f1f4b08e6
Cwickks <69566792+Cwickks@users.noreply.github.com>
2023-05-03 22:03:10 +0800

    basic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const fastify = require("fastify")();
+const cors = require("fastify-cors");
 const multer = require("multer");
 const os = require("os");
 
@@ -16,29 +17,27 @@ const upload = multer({
   storage: storage,
 });
 
-(async () => {
-  await fastify.register(require("@fastify/cors"), {
-    origin: "*",
-  });
+fastify.register(cors, {
+  origin: "*",
+});
 
-  fastify.setErrorHandler(async (error, request, reply) => {
-    console.log(error);
-    reply.status(500).send("Internal server error");
-  });
+fastify.setErrorHandler(async (error, request, reply) => {
+  console.log(error);
+  reply.status(500).send("Internal server error");
+});
 
-  fastify.get("/", (request, reply) => {
-    reply.send("OK");
-  });
+fastify.get("/", (request, reply) => {
+  reply.send("OK");
+});
 
-  fastify.get("/animetracker", async (request, reply) => {
-    if (request.query.key !== process.env.KEY) return reply.code(403).send("Invalid key");
-    await require("./api/animetracker.js")(request, reply);
-  });
+fastify.get("/animetracker", async (request, reply) => {
+  if (request.query.key !== process.env.KEY) return reply.code(403).send("Invalid key");
+  await require("./api/animetracker.js")(request, reply);
+});
 
-  fastify.post("/upload", { preHandler: upload.array("files") }, async (request, reply) => {
-    if (request?.files?.length) return reply.code(400).send("No files specified");
-    await require("./api/upload.js")(request, reply);
-  });
+fastify.post("/upload", { preHandler: upload.array("files") }, async (request, reply) => {
+  if (request?.files?.length) return reply.code(400).send("No files specified");
+  await require("./api/upload.js")(request, reply);
+});
 
-  fastify.listen({ port: process.env.PORT });
-})();
+fastify.listen({ port: process.env.PORT });
