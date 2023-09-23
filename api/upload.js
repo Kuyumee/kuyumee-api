2ea0d021034c63d34c9e7ad72b5836ec3bec5151
Kuyumee <69566792+Kuyumee@users.noreply.github.com>
2023-09-23 11:15:45 +0800

    added single file support maybe

--- api/upload.js
+++ api/upload.js
@@ -1,3 +1,5 @@
+const { send } = require("process");
+
 async function upload(request, reply) {
   let files;
 
@@ -10,15 +12,41 @@ async function upload(request, reply) {
     return;
   }
 
-  console.log("Loading modules");
+  const bucket = require("../helpers/bucket.js");
+
+  if (files.length === 1) {
+    const { filepath, filename } = files[0];
+    const url = await bucket.upload(filepath, filename);
+    sendToDiscord(`${request.headers["x-forwarded-for"]} uploaded ${filename} to ${url}`);
+  } else if (files.length > 1) {
+    const { zipPath, zipName } = makeZip(files);
+    const url = await bucket.upload(zipPath, zipName);
+    sendToDiscord(`${request.headers["x-forwarded-for"]} uploaded ${files.length} files to ${url}`);
+  }
+
+  reply.send("OK");
+}
+
+async function sendToDiscord(message) {
+  const axios = require("axios");
+
+  const webhook = process.env.DISCORD_WEBHOOK;
+  if (!webhook) return;
+
+  await axios(process.env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    data: {
+      content: message,
+    },
+  });
+}
+
+async function makeZip(files) {
   const os = require("os");
   const path = require("path");
   const fs = require("fs-extra");
   const archiver = require("archiver");
-  const axios = require("axios");
-  const bucket = require("../helpers/bucket.js");
 
-  console.log("Creating zip");
   const archive = archiver("zip", {
     zlib: { level: 0 },
   });
@@ -29,32 +57,21 @@ async function upload(request, reply) {
 
   archive.pipe(zipWriteStream);
 
-  console.log("Adding files to zip");
   for (const file of files) {
-    const dateMatch = file.filename.match(/(\d{4})\D?(\d{2})\D?(\d{2})\D?(\d{2})\D?(\d{2})\D?(\d{2})/);
-    const restoredDate = dateMatch ? new Date(Date.UTC(dateMatch[1], dateMatch[2] - 1, dateMatch[3], dateMatch[4], dateMatch[5], dateMatch[6])) : new Date();
-    fs.utimesSync(file.filepath, restoredDate, restoredDate);
-
+    restoreDate(file.filepath, file.filename);
     archive.file(file.filepath, { name: file.filename });
   }
 
-  console.log("Finalizing zip");
   await archive.finalize();
   await new Promise((res) => zipWriteStream.on("close", res));
 
-  console.log("Uploading zip");
-  const url = await bucket.upload(zipPath, zipName);
+  return { zipPath, zipName };
+}
 
-  fs.removeSync(zipPath);
-
-  await axios(process.env.DISCORD_WEBHOOK_URL, {
-    method: "POST",
-    data: {
-      content: `${request.headers["x-forwarded-for"]} uploaded ${files.length} files to ${url}`,
-    },
-  });
-
-  reply.send("OK");
+async function restoreDate(file) {
+  const dateMatch = file.filename.match(/(\d{4})\D?(\d{2})\D?(\d{2})\D?(\d{2})\D?(\d{2})\D?(\d{2})/);
+  const restoredDate = dateMatch ? new Date(Date.UTC(dateMatch[1], dateMatch[2] - 1, dateMatch[3], dateMatch[4], dateMatch[5], dateMatch[6])) : new Date();
+  fs.utimesSync(file.filepath, restoredDate, restoredDate);
 }
 
 module.exports = upload;
